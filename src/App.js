6147e47b826edd0ce9dad52a141d1f571048f13f
minjurRahman <minjur.rahaman99@gmail.com>
2022-10-11 17:39:14 +0600

    Error Page added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './components/Main/Main';
+import Home from './components/Home';
+import ErrorPage from './components/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path:'/',
     element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
     children:[
       {
         path:'/',
-        
-      }
+        element: <Home></Home>,
+        loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+      },
     ]
   }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import { Outlet, useLoaderData } from 'react-router-dom';
 import Header from '../Header';
 
 
 const Main = () => {
+    const quizeTopics = useLoaderData()
     return (
         <div>
 
             <Header></Header>
+            <Outlet></Outlet>
 
-        </div>
+        </div >
     );
 };
 
